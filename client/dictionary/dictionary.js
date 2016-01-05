a7a3201462209faa0b69c811a90c6aa49c070590
imrokas <rokas@imrokas.com>
2016-01-05 14:27:23 -0500

    added ability to delete words

--- client/dictionary/dictionary.js
+++ client/dictionary/dictionary.js
@@ -1,22 +1,20 @@
 angular.module('wordHoarder.dictionaryController', [])
 .controller('DictionaryController', function($scope, FirebaseFactory, $firebaseArray) {
-	$scope.addWord = [];
-	$firebaseArray(FirebaseFactory.getRef()).$loaded().then(function(arr){
-		console.log('arr before filter: ', arr);
+	$scope.allWords = [];
+	var list = $firebaseArray(FirebaseFactory.getRef());
+	list.$loaded().then(function(arr){
 		$scope.allWords = arr.filter(function(word){
 			return word.uid === FirebaseFactory.getRef().getAuth().uid;
 		});
-		console.log('Words loaded: ', $scope.allWords);
 	});
-	//$scope.allWords = FirebaseFactory.getAllWords();
 	$scope.addWord = function(data) {
 		data.uid = FirebaseFactory.getRef().getAuth().uid;
-		console.log('data = ', data);
-		$firebaseArray(FirebaseFactory.getRef()).$add(data);
+		list.$add(data);
 	};
 
-	$scope.deleteWord = function(data) {
-		console.log('delete data = ', data);
-		$firebaseArray(FirebaseFactory.getRef()).$remove(data);
+	$scope.deleteWord = function(data, $index) {
+		list.$remove(data).then(function(ref) {
+			$scope.allWords.splice($index,1);
+		});
 	};
 });
