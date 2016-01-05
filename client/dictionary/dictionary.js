c8b007efc1e1fd8a44c467b5d36fa332bf766a75
imrokas <rokas@imrokas.com>
2016-01-05 13:50:54 -0500

    added ability to add words and see all words added by the user

--- client/dictionary/dictionary.js
+++ client/dictionary/dictionary.js
@@ -1,4 +1,22 @@
 angular.module('wordHoarder.dictionaryController', [])
-.controller('DictionaryController', function($scope, AuthenticationFactory) {
+.controller('DictionaryController', function($scope, FirebaseFactory, $firebaseArray) {
+	$scope.addWord = [];
+	$firebaseArray(FirebaseFactory.getRef()).$loaded().then(function(arr){
+		console.log('arr before filter: ', arr);
+		$scope.allWords = arr.filter(function(word){
+			return word.uid === FirebaseFactory.getRef().getAuth().uid;
+		});
+		console.log('Words loaded: ', $scope.allWords);
+	});
+	//$scope.allWords = FirebaseFactory.getAllWords();
+	$scope.addWord = function(data) {
+		data.uid = FirebaseFactory.getRef().getAuth().uid;
+		console.log('data = ', data);
+		$firebaseArray(FirebaseFactory.getRef()).$add(data);
+	};
 
+	$scope.deleteWord = function(data) {
+		console.log('delete data = ', data);
+		$firebaseArray(FirebaseFactory.getRef()).$remove(data);
+	};
 });
--- client/services/services.js
+++ client/services/services.js
@@ -54,71 +54,33 @@ angular.module('wordHoarder.services', [])
 		logout: logout
 	};	
 })
-.factory('FirebaseFactory', function() {
+.factory('FirebaseFactory', function($firebaseArray) {
 	var firebaseUrl = "https://wordhoarder.firebaseio.com";
 	var ref = new Firebase(firebaseUrl);
+	var list = $firebaseArray(ref);
 	//return reference to firebase
 	var getRef = function() {
 		return ref;
 	};
+
+	// var addWord = function(data) {
+	// 	data.uid = ref.getAuth.uid;
+	// 	list.$add(data).then(function(ref) {
+	// 		console.log('word: ' + data + ' added with id ' + ref);
+	// 	});
+	// };
+
+	// var getAllWords = function() {
+	// 	return list.$loaded().then(function(){
+	// 		return list.filter(function(word){
+	// 			return word.uid === ref.getAuth().uid;
+	// 		});
+	// 	});
+	// };
+
 	return {
-		getRef: getRef
+		getRef: getRef,
+		//add: addWord,
+		//getAllWords: getAllWords
 	};
 });
-
-// var ref = new Firebase("https://wordhoarder.firebaseio.com");
-// 	var authObj = $firebaseAuth(ref);
-// 	$scope.register = function(user) {
-// 		ref.createUser({
-// 		  email    : user.email,
-// 		  password : user.password
-// 		}, function(error, userData) {
-// 		  if (error) {
-// 		    console.log("Error creating user:", error);
-// 		    $scope.reg = true;
-// 		    $scope.msg = error;
-// 		  } else {
-// 		    console.log("Successfully created user account with uid:", userData.uid);
-// 		    $scope.msg = userData.uid;
-// 		    $scope.reg = true;
-// 		  }
-// 		});
-// 		user.email = '';
-// 		user.password = '';
-// 	};
-
-// 	$scope.loggedIn = function() {
-// 		if(ref.getAuth()) {
-// 			$scope.loggedInMsg = "user is logged in.";
-// 			console.log("user is logged in.");
-// 			return true;
-// 		}
-// 		$scope.loggedOutMsg = "user is logged out.";
-// 		console.log("user is logged out.");
-// 		return false;
-// 	}
-
-// 	$scope.login = function(user) {
-// 		ref.authWithPassword({
-// 		  email    : user.email,
-// 		  password : user.password
-// 		}, function(error, authData) {
-// 		  if (error) {
-// 		    console.log("Login Failed!", error);
-// 		  } else {
-// 		  	$scope.log = true;
-// 		  	$scope.msg = "Logged in as:" + authData.uid;
-// 		    console.log("Authenticated successfully with payload:", authData);
-// 		  }
-// 		}, {
-// 			remember: 'sessionOnly'
-// 		});
-// 		user.email = '';
-// 		user.password = '';
-// 	};
-
-// 	$scope.logout = function() {
-// 		console.log('inside logout');
-// 		ref.unauth();
-// 		console.log('ref = ', ref);
-// 	}
